fef2fbd12453dfdb9a2cfa739f3542e2c00afb01
An Tran <tranlaman@gmail.com>
2016-01-07 14:32:05 +0800

    outliers chapter

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -2,18 +2,23 @@
 
 import pickle
 import sys
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+data_dict.pop('TOTAL', 0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    plt.scatter(salary, bonus)
 
-
-
+plt.xlabel('salary')
+plt.ylabel('bonus')
+plt.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -14,7 +14,20 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    num_samples = len(ages)
+    errors = np.empty((num_samples))
+    for ind in xrange(num_samples):
+        errors[ind] = abs(predictions[ind] - net_worths[ind])
 
+    # sort and get a pivot value
+    sorted_errors = errors.copy()
+    sorted_errors.sort()
+    pivot_ind = int(0.9 * num_samples)
+    pivot_value = sorted_errors[pivot_ind]
     
-    return cleaned_data
+    keep = errors < pivot_value
+    for ind in xrange(num_samples):
+        if keep[ind]:
+            cleaned_data.append((ages[ind], net_worths[ind], errors[ind]))
 
+    return cleaned_data
